97971d1f795c6767ddf79ce4d316a1c874e17080
Hunter John Larco <admin@larcolabs.com>
2015-10-12 12:29:59 -0700

    User collection without authentification

--- server.py
+++ server.py
@@ -2,7 +2,12 @@ from flask import Flask, request, make_response, jsonify
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from utils.mongo_json_encoder import JSONEncoder
+from functools import wraps
+
+
+
 
 # Basic Setup
 app = Flask(__name__)
@@ -10,31 +15,48 @@ mongo = MongoClient('localhost', 27017)
 app.db = mongo.develop_database
 api = Api(app)
 
+
+
+
 #Implement REST Resource
-class MyObject(Resource):
+class User(Resource):
 
-    def post(self):
-      new_myobject = request.json
-      myobject_collection = app.db.myobjects
-      result = myobject_collection.insert_one(request.json)
+  # @requires_auth
+  def post(self):
+    json = request.json
+    collection = app.db.users
+    addition = collection.insert_one(json)
 
-      myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
+    entity = collection.find_one({"_id": ObjectId(addition.inserted_id)})
+    return entity
 
-      return myobject
+  def get(self, user_id):
+    collection = app.db.users
+    
+    try:
+      entity = collection.find_one({"_id": ObjectId(user_id)})
+    except InvalidId:
+      response = jsonify(data=[])
+      response.status_code = 404
+      return response
+      
 
-    def get(self, myobject_id):
-      myobject_collection = app.db.myobjects
-      myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
+    if not entity:
+      response = jsonify(data=[])
+      response.status_code = 404
+      return response
+    else:
+      return entity
 
-      if myobject is None:
-        response = jsonify(data=[])
-        response.status_code = 404
-        return response
-      else:
-        return myobject
+
+
+
 
 # Add REST resource to API
-api.add_resource(MyObject, '/myobject/','/myobject/<string:myobject_id>')
+api.add_resource(User, '/users', '/users/', '/users/<string:user_id>')
+
+
+
 
 # provide a custom JSON serializer for flaks_restful
 @api.representation('application/json')
@@ -42,6 +64,9 @@ def output_json(data, code, headers=None):
     resp = make_response(JSONEncoder().encode(data), code)
     resp.headers.extend(headers or {})
     return resp
+
+
+
 
 if __name__ == '__main__':
     # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
