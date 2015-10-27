0eb525ed6f9ce0181d1d30d405aff2575d39bb17
Ben-G <Benjamin.Encz@gmail.com>
2015-10-26 18:30:49 -0700

    Code Review I

--- dbmodels.py
+++ dbmodels.py
@@ -5,20 +5,25 @@ import bcrypt
 """ LOCAL IMPORTS """
 import db
 
+# [Ben-G] TODO: I create separate files for the User and Trip model
 
 class UserModel(db.Model):
   """
   ' PURPOSE
   '   Contains data for all users
   """
-  
+
   """ PROPERTIES """
   email = db.StringProperty()
   password = db.ByteStringProperty()
-  
+
   """ CONSTANTS """
+  # [Ben-G] TODO: It would be greate if this were a property of your
+  # application that way your tests can access and modify the bcrypt rounds.
+  # You want tests to run blazingly fast so you should set the rounds to the
+  # minimum possible of 8.
   BCRYPT_ROUNDS = 12
-  
+
   def hash_password(self, password):
     """
     ' PURPOSE
@@ -31,7 +36,7 @@ class UserModel(db.Model):
     password = password.encode('utf-8')
     hashed = bcrypt.hashpw(password, bcrypt.gensalt(self.BCRYPT_ROUNDS))
     return hashed
-  
+
   def set_password(self, password):
     """
     ' PURPOSE
@@ -44,7 +49,7 @@ class UserModel(db.Model):
     '   Nothing
     """
     self.password = self.hash_password(password)
-  
+
   def check_password(self, password):
     """
     ' PURPOSE
@@ -58,7 +63,7 @@ class UserModel(db.Model):
     """
     password = password.encode('utf-8')
     return bcrypt.hashpw(password, self.password) == self.password
-  
+
   def to_dict(self):
     """
     ' PURPOSE
@@ -69,21 +74,24 @@ class UserModel(db.Model):
     '   dict( email )
     '     email -> the user's email
     """
+    # [Ben-G] TODO: Minor note: you shoudn't need to implement this method
+    # yourself. You should be able to use the `__dict__` method or other
+    # python metaprogramming tools to generate the dictionary automatically
     return {
       'email': self.email
     }
-    
+
 
 class TripModel(db.Model):
   """
   ' PURPOSE
   '   Contains data for all trips
   """
-  
+
   """ PROPERTIES """
   name = db.StringProperty()
   author = db.KeyProperty()
-  
+
   def to_dict(self):
     """
     ' PURPOSE
@@ -95,6 +103,9 @@ class TripModel(db.Model):
     '     name -> the name of this trip
     '     author -> the identifier of the author of this trip
     """
+    # [Ben-G] TODO: Minor note: you shoudn't need to implement this method
+    # yourself. You should be able to use the `__dict__` method or other
+    # python metaprogramming tools to generate the dictionary automatically
     return {
       'name': self.name,
       'author': self.author.id
